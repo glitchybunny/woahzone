6831020702769ba8a9b3136df0cc6962c3b73104
darkrilin <darkrilin@users.noreply.github.com>
2020-05-15 00:17:47 +1000

    please let me test localhost

--- index.js
+++ index.js
@@ -82,3 +82,12 @@ SimpleSignalServer.prototype._onDisconnect = function (socket) {
   }
   this.emit('disconnect', socket)
 }
+
+// enable CORS so it's easier to test and also because the server isn't run on rtay.io
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method");
+	res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+	res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+	next();
+});
