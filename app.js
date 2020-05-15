3f2b16cdc8ae3c5f7a790bc93226d8a84d34c907
Bunny <12066089+glitchybunny@users.noreply.github.com>
2020-05-15 17:15:08 +1000

    Migrated client to different site, this is just server

--- app.js
+++ app.js
@@ -1,24 +1,10 @@
 // Including libraries
 
+const cors = require('cors');
 const app = require('http').createServer(handler);
 const io = require('socket.io').listen(app);
-const static = require('node-static'); // for serving files
 
-// This will make all the files in the current folder
-// accessible from the web
-const fileServer = new static.Server('./');
-	
-// This is the port for our web server.
-// you will need to go to http://localhost:8080 to see it
-app.listen(8080);
-
-// If the URL of the socket server is opened in a browser
-function handler (request, response) {
-
-	request.addListener('end', function () {
-        fileServer.serve(request, response);
-    });
-}
+app.options('*', cors())
 
 // Listen for incoming connections from clients
 io.on('connection', function (socket) {
