b8867df52c91d6ca9e7e55dd53540b40ca5be078
Bunny <12066089+glitchybunny@users.noreply.github.com>
2020-05-15 14:32:41 +1000

    Not sure what I'm even doing god help us

--- index.js
+++ index.js
@@ -3,22 +3,21 @@ const signalServer = require('simple-signal-server')(io);
 const allIDs = new Set();
 
 // Configure socket.io to work with CORS
-io.configure('production', function(){
-    console.log("Server in production mode");
-    io.enable('browser client minification');  // send minified client
-    io.enable('browser client etag'); // apply etag caching logic based on version number
-    io.enable('browser client gzip'); // the file
-    io.set('log level', 1);           // logging
-    io.set('transports', [            // all transports (optional if you want flashsocket)
-        'websocket',
-		'flashsocket',
-		'htmlfile',
-		'xhr-polling',
-		'jsonp-polling'
-    ]);
-	io.set('origins', "http://localhost:* http://127.0.0.1:* https://rtay.io:*");
-});
+console.log("Server in production mode");
+io.enable('browser client minification');  // send minified client
+io.enable('browser client etag'); // apply etag caching logic based on version number
+io.enable('browser client gzip'); // the file
+io.set('log level', 1);           // logging
+io.set('transports', [            // all transports (optional if you want flashsocket)
+	'websocket',
+	'flashsocket',
+	'htmlfile',
+	'xhr-polling',
+	'jsonp-polling'
+]);
+io.set('origins', "http://localhost:* http://127.0.0.1:* https://rtay.io:*");
 
+// Handle server behaviour
 signalServer.on('discover', (request) => {
 	const clientID = request.socket.id; // you can use any kind of identity, here we use socket.id
 	allIDs.add(clientID); // keep track of all connected peers
