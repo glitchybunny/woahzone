0e59f7bb4dacfa9aa6c95e6a84e40e6e11e3a598
Bunny <12066089+glitchybunny@users.noreply.github.com>
2020-05-27 19:00:05 +1000

    Version 1 of the multiplayer server is done
    Sorry that it's super messy ugly code, I'm just happy it works!

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const http = require('http');
+const xss = require("xss");
 var path = require('path');
 
+// Cors proofing
 const cors = require('cors');
 var allowCrossDomain = function(req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*');
@@ -22,28 +24,90 @@ var allowCrossDomain = function(req, res, next) {
 app.use(cors());
 app.use(allowCrossDomain);
 
-// Listen for incoming connections from clients
-io.on('connection', (client) => {
-	console.log("Client connected: ", client.id);
+// Basic client list (to keep track of currently connected client IDs and names only, nothing more)
+const clients = [];
+const sockets = [];
 
-	client.on('join', function(data) {
-		console.log(data);
+// Listen for incoming connections from clients
+io.on('connection', (socket) => {
+
+	clients[socket.id] = {};
+	sockets[socket.id] = socket;
+
+	// Listen for disconnect events
+	socket.on('disconnecting', (data) => {
+		// Only broadcast to other clients that this one has left IF it has triggered a join event
+		if (clients[socket.id].id !== undefined) {
+			socket.broadcast.emit('leave', clients[socket.id].id);
+		}
+		delete clients[socket.id];
 	});
 
-	// Start listening for mouse move events
-	client.on('mousemove', function (data) {
-		
-		// This line sends the event (broadcasts it)
-		// to everyone except the originating client.
-		client.broadcast.emit('moving', data);
+	// Listen for client joining and let other clients know to broadcast their names
+	socket.on('join', (data) => {
+		// Make sure name is XSS safe
+		let _id = data.id || 0;
+		let _name = data.name || "";
+		_name = xss(_name.substr(0, 20));
+
+		if (_id !== 0) {
+			// Add data to current client list
+			clients[socket.id].id = _id;
+			clients[socket.id].name = _name;
+
+			// Broadcast join to all others (which will then respond with name for original client)
+			socket.broadcast.emit('join', clients[socket.id]);
+
+			// Note that client has joined in the console
+			console.log(_id, "joined with name:", _name);
+		}
+	});
+
+	// Listen for name change events
+	socket.on('name', (data) => {
+		// Make sure name is XSS safe
+		let _id = data.id;
+		let _name = data.name;
+		_name = xss(_name.substr(0, 20));
+
+		// Update name in client list
+		clients[socket.id].name = _name;
+
+		// If there's a target, only send the name to that target
+		if (data.target !== undefined) {
+			// Iterate over connected clients to find target and send them the message
+			for (let c in clients) {
+				if (clients[c].id == data.target) {
+					sockets[c].emit('name', {
+						id: _id,
+						name: _name
+					})
+				}
+			}
+		} else {
+			// Otherwise broadcast name change to all other users
+			socket.broadcast.emit('name', {
+				id: _id,
+				name: _name
+			});
+		}
+	});
+
+	// Listen for movement events
+	socket.on('move', (data) => {
+		console.log(data);
+		socket.broadcast.emit('move', data);
 	});
 });
+
 
 // Http server
 app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname + '/index.html'));
 });
-const files = ['/js/socket.io.js', '/js/woahzone.js', '/js/WebGL.min.js'];
+const files = ['/js/socket.io.js', '/js/woahzone.js', '/js/WebGL.min.js', '/js/three.module.js', '/js/controls/PointerLockControls.js',
+	'/js/loaders/GLTFLoader.js', '/meshes/test1.glb', '/img/skybox/cloudtop_ft.jpg', '/img/skybox/cloudtop_bk.jpg', '/img/skybox/cloudtop_up.jpg',
+	'/img/skybox/cloudtop_dn.jpg', '/img/skybox/cloudtop_lf.jpg', '/img/skybox/cloudtop_rt.jpg', '/meshes/player.glb',];
 for (let i in files) {
 	app.get(files[i], function(req, res) {res.sendFile(path.join(__dirname + files[i]));});
 }
