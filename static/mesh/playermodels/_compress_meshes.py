514a83e9c6fb1f1fc2c30e690b63291e4e7f8499
Riley Taylor <darkrilin@users.noreply.github.com>
2020-10-17 13:19:14 +1100

    Added some spooky skins to the spookzone

---
+++ static/mesh/playermodels/_compress_meshes.py
@@ -0,0 +1,11 @@
+import os
+
+files = []
+
+directory = os.path.dirname(os.path.realpath(__file__))
+for filename in os.listdir(directory):
+    if filename.endswith(".glb") and not filename.endswith(".min.glb"):
+        files.append(filename[:-4])
+
+for i in files:
+    os.system('gltf-pipeline -i ' + i + '.glb -o ' + i + '.min.glb -d --draco.compressionLevel 8 --draco.quantizePositionBits 15 --draco.unifiedQuantization true --draco.quantizeTexcoordBits 14')
